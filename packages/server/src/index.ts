9d4f9734650fab7d6311a1fc358d6871eb323b73
Ash Mitchell <amitch35@calpoly.edu>
2024-05-11 17:43:22 -0700

    Add Middleware to remaining API

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import path from "path";
-import {supabaseClient} from './utils/supabase/server';
 import properties from './routes/properties';
 import staff from './routes/staff';
 import roles from './routes/roles';
@@ -37,29 +36,17 @@ const nodeModules = path.resolve(
 console.log("Serving NPM packages from", nodeModules);
 app.use("/node_modules", express.static(nodeModules));
 
-const supabase = supabaseClient;
-
 app.use('/auth', auth);
 
-app.use('/api/properties', properties);
+app.use('/api/properties', supabaseMiddleware, properties);
 
-app.use('/api/staff', staff);
+app.use('/api/staff', supabaseMiddleware, staff);
 
 app.use('/api/roles', supabaseMiddleware, roles);
 
-app.use('/api/appointments', appointments);
+app.use('/api/appointments', supabaseMiddleware, appointments);
 
-app.use('/api/plans', plans)
-
-// Local User Session
-app.get('/api/user-session', async (req: Request, res: Response) => {
-  const { data, error } = await supabase.auth.getSession()
-  if (error) {
-    res.send(error);
-  } else {
-    res.send(data);
-  }
-});
+app.use('/api/plans', supabaseMiddleware, plans)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
